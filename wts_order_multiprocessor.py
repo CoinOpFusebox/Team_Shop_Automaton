95eeed1cdff3d602c5e97b02bde9252445bea2a8
CoinOpFusebox <85265229+CoinOpFusebox@users.noreply.github.com>
2024-05-06 15:49:25 -0500

    Updated to remove depreciated database functionality

--- wts_order_multiprocessor.py
+++ wts_order_multiprocessor.py
@@ -1,21 +1,19 @@
 # ---------------------------------------------------------------------------------------------------------------------#
 # wts_order_multiprocessor.py
 #
-# This module runs the wts_order_processor until it runs out of orders. It also runs the inbox highlanderer and preorder
-# data logger before it starts running orders.
+# This module runs the wts_order_processor until it runs out of orders. It runs the inbox highlanderer first to weed
+# out duplicate emails.
 #
 # I'll rename this when I think of a better name.
 #
 # ---------------------------------------------------------------------------------------------------------------------#
 
 import inbox_highlanderer
-import preorder_data_logger
 import wts_order_processor
 
 from time import sleep
 
 inbox_highlanderer.main()
-preorder_data_logger.main()
 
 persist = True
 
