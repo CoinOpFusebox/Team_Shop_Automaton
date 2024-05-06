ebb5ad157d34bcd1438c5fdad47bf4ed716fa2db
CoinOpFusebox <85265229+CoinOpFusebox@users.noreply.github.com>
2024-05-06 15:44:12 -0500

    Updated to change naming source from preorder db to WTS Status

--- folder_molder.py
+++ folder_molder.py
@@ -1,35 +1,33 @@
 # ---------------------------------------------------------------------------------------------------------------------#
 # folder_molder.py
 #
-# This module makes folders from Team Shop emails. It does this by referencing the TeamShopDB for accurate folder names.
+# This module makes folders from Team Shop emails.
 #
 # ---------------------------------------------------------------------------------------------------------------------#
 
-import apsw
 import csv
 import os
+import pyodbc
 import re
 import win32com.client
 
 from configparser import ConfigParser
 from datetime import datetime
+from dotenv import load_dotenv
 from pathlib import Path
 
 
 def main():
-    # This block opens the configuration file and retrieves the database and working folder paths.
+    # This block opens the configuration file and retrieves the working folder path.
 
     config_path = Path(__file__).parent.absolute().joinpath('config.ini')
     config = ConfigParser()
     config.read(config_path)
 
-    database_path = config['Folder Paths']['database_path']
     working_folder_path = config['Folder Paths']['working_folder_path']
 
-    # Here, the database connection is opened, as is Outlook, and email folders are defined for later use.
-
-    connection = apsw.Connection(database_path)
-    cursor = connection.cursor()
+    # This opens Outlook and defines email folders for later use.
+    # TODO: Stop using the wrong archive!! WHY IS THIS SO HARD!?
 
     outlook = win32com.client.Dispatch('Outlook.Application')
     mapi = outlook.GetNamespace('MAPI')
@@ -47,15 +45,9 @@ def main():
     try:
         last_message = messages[0]
     except IndexError:
-        # print('No emails!')
         return 'No emails!'
 
     count_file = None
-    database_tuple = None
-
-    # This block creates a useful string for later.
-
-    year_awp_cwp = ''.join((str(datetime.now().year)[-2:], 'AWP CWP'))
 
     # This creates a temporary copy of the count file so it can be examined for length.
     # If it has fewer than ten lines, it's blank and can be archived along with the email.
@@ -73,7 +65,7 @@ def main():
 
             line_count = 0
 
-            for line in reader:
+            for __ in reader:
                 line_count += 1
 
         if line_count < 10:
@@ -84,34 +76,42 @@ def main():
         else:
             os.remove(temp_path)
 
-    # This checks the database for a matching store id.
-    # If it doesn't find one, it uses the title of the email instead. Might as well try!
+    # This gets the store id from the email subject.
 
     store_id_match = re.search(r'\d\d\d\d\d\d\d\d\d\d', last_message.Subject)
     store_id = store_id_match[0]
 
-    cursor.execute('SELECT * FROM team_name_index WHERE store_id=?', (store_id,))
-    database_tuple = cursor.fetchone()
+    # This gets the team name from the WTS Status database using the store id.
+    # If it doesn't find one, it uses the title of the email instead. Might as well try!
 
-    missing_name = False
+    load_dotenv()
 
-    if not database_tuple:
-        missing_name = True
+    connection_list = ['DSN=WTS Status;Database=WTS Status;UID=',
+                       os.getenv('WTSS_UID'),
+                       ';PWD=',
+                       os.getenv('WTSS_PWD')]
 
-    # This creates the folder.
+    connection = pyodbc.connect(''.join(connection_list))
+    cursor = connection.cursor()
 
-    if missing_name:
-        shop_folder_number = str(last_message.Subject).split('-')[1]
-        team_folder_name = ''.join((str(last_message.Subject).split('-')[0], ' ', year_awp_cwp))
-    else:
-        shop_folder_number = str(database_tuple[0])
-        team_folder_name = database_tuple[1].strip()
+    try:
+        team_name = cursor.execute('SELECT "Team Name" FROM "WTS Status" WHERE "Store ID 2"=?', store_id).fetchval()
+    except TypeError:
+        team_name = str(last_message.Subject).split('-')[0]
 
-        if 'CWP AWP' in team_folder_name:
-            team_folder_name = team_folder_name.replace('CWP AWP', 'AWP CWP')
+    if '  ' in team_name:
+        team_name = team_name.replace('  ', ' ')
 
-    full_path = ''.join((working_folder_path, '\\', team_folder_name, '\\', shop_folder_number))
-    os.makedirs(full_path, exist_ok=True)
+    # This creates the folders.
+
+    year_awp_cwp = ''.join((' ', str(datetime.now().year)[-2:], 'AWP CWP'))
+
+    full_path = ''.join((working_folder_path, '\\', team_name, year_awp_cwp, '\\', store_id))
+    try:
+        os.makedirs(full_path, exist_ok=True)
+    except ValueError:
+        full_path = full_path.replace('\x00', '')
+        os.makedirs(full_path, exist_ok=True)
 
     # This puts a copy of the count sheet in the folder.
 
